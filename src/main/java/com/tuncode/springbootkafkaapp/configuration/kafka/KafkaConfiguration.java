39a3e7ad58ea417357e3333a639c972a58155a72
Cem Tunc Aksuna <65118839+tuncAksuna@users.noreply.github.com>
2025-03-04 11:04:03 +0300

    Update KafkaConfiguration.java

--- src/main/java/com/tuncode/springbootkafkaapp/configuration/kafka/KafkaConfiguration.java
+++ src/main/java/com/tuncode/springbootkafkaapp/configuration/kafka/KafkaConfiguration.java
@@ -19,7 +19,6 @@ import java.util.Map;
 /**
  * @author caksuna on 14.05.2024 12:51
  */
-
 @Configuration
 public class KafkaConfiguration {
 
@@ -30,47 +29,46 @@ public class KafkaConfiguration {
     private String consumerGroupId;
 
     @Bean
-    public KafkaTemplate<String, ?> tyKafkaTemplate() {
-        return new KafkaTemplate<>(producerConfig());
-    }
-
-    private ProducerFactory<String, ?> producerConfig() {
+    public ProducerFactory<String, Object> producerFactory() {
         Map<String, Object> config = new HashMap<>();
         config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaAddresses);
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         config.put(ProducerConfig.ACKS_CONFIG, "1");
-        config.put(ProducerConfig.RETRIES_CONFIG, "10");
+        config.put(ProducerConfig.RETRIES_CONFIG, 10);
+
         return new DefaultKafkaProducerFactory<>(config);
+    }
+
+    @Bean
+    public KafkaTemplate<String, Object> tyKafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
     }
 
     @Bean
     public ConsumerFactory<String, Object> consumerFactory() {
         Map<String, Object> props = new HashMap<>();
-
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaAddresses);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, consumerGroupId);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
         props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
-        props.put(ConsumerConfig.HEARTBEAT_INTERVAL_MS_CONFIG, "5000"); // hearbeat config for session timeout config
-        props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, 15000);     // if the application does not send a hearbeat for 15 seconds, the consumer will be considered dead.
+        props.put(ConsumerConfig.HEARTBEAT_INTERVAL_MS_CONFIG, 5000);
+        props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, 15000);
 
-        JsonDeserializer<Object> payloadJsonDeserializer = new JsonDeserializer<>();
-        payloadJsonDeserializer.addTrustedPackages("*");
+        JsonDeserializer<Object> payloadJsonDeserializer = new JsonDeserializer<>(Object.class);
+        payloadJsonDeserializer.addTrustedPackages("com.ornek.model");
+        payloadJsonDeserializer.setRemoveTypeHeaders(false);
+        payloadJsonDeserializer.setUseTypeMapperForKey(true);
 
-        return new DefaultKafkaConsumerFactory<>(props,
-                new StringDeserializer(),
-                payloadJsonDeserializer);
+        return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), payloadJsonDeserializer);
     }
 
     @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
-        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+    public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
         factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
         return factory;
     }
-
 }
-
